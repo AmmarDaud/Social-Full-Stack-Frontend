fefbdf34596bcf5576e0fd1649b619fe903694dd
Ammar Daud <ammardaud12@gmail.com>
2025-07-24 15:03:08 -0400

    CHANGES ADDED

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="bg-gray-800 p-4 flex space-x-4 text-white">
-      <NavLink to="/" className="hover:text-yellow-400">Home</NavLink>
-      <NavLink to="/register" className="hover:text-yellow-400">Register</NavLink>
-      <NavLink to="/signin" className="hover:text-yellow-400">Sign In</NavLink>
-      <NavLink to="/feed" className="hover:text-yellow-400">Feed</NavLink>
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
+        <li><NavLink to="/login">Sign In</NavLink></li>
+      </ul>
     </nav>
   );
 }
+
 export default NavBar;
